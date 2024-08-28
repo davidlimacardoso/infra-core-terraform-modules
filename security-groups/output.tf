c6af43967916605410f98f66b95db0155cb2a6ea
David Cardoso <david.lima.cd@gmail.com>
2024-08-28 16:11:26 -0300

    Revert "Add module of security group"

--- security-groups/output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = { for k, v in aws_security_group.create_sg : k => v.id }
-  description = "Security group ID's"
-}
-
-output "name" {
-  value       = { for k, v in aws_security_group.create_sg : k => v.name }
-  description = "Security group names"
-}
